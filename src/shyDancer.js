6e9ec55137558dacc94a3565952eac09a27acfc9
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-19 13:05:37 -0700

    Added cute gif for shy dancer and made her run faster.

--- src/shyDancer.js
+++ src/shyDancer.js
@@ -10,8 +10,8 @@ ShyDancer.prototype = Object.create(Dancer.prototype);
 ShyDancer.prototype.constructor = ShyDancer;
 
 ShyDancer.prototype.run = function () {
-  this.top += Math.random() * 200 - 100;
-  this.left += Math.random() * 200 - 100;
+  this.top += Math.random() * 300 - 150;
+  this.left += Math.random() * 300 - 150;
 
   this.$node.animate({top: this.top, left: this.left});
 };
