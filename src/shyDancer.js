7151f8d6d005f3c487af75b48e239d517ae9dbd7
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-19 12:47:02 -0700

    Added shyDancer that doesn't do anything when step.

---
+++ src/shyDancer.js
@@ -0,0 +1,16 @@
+var ShyDancer = function(top, left, timeBetweenSteps){
+
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  this.$node.addClass('shy-dancer');
+};
+
+ShyDancer.prototype = Object.create(Dancer.prototype);
+ShyDancer.prototype.constructor = ShyDancer;
+
+ShyDancer.prototype.run = function () {
+  this.top += Math.random() * 200 - 100;
+  this.left += Math.random() * 200 - 100;
+
+  this.$node.animate({top: this.top, left: this.left});
+};
