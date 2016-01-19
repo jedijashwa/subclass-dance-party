f743af44bf1926bb889a7b3c5f330330b3525eba
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-19 12:23:28 -0700

    Added test for badDancer moving on step.

--- spec/badDancerSpec.js
+++ spec/badDancerSpec.js
@@ -13,12 +13,6 @@ describe("badDancer", function() {
     expect(badDancer.$node).to.be.an.instanceof(jQuery);
   });
 
-  it("should have a step function that makes its node blink", function() {
-    sinon.spy(badDancer.$node, 'toggle');
-    badDancer.step();
-    expect(badDancer.$node.toggle.called).to.be.true;
-  });
-
   describe("dance", function(){
     it("should call step at least once per second", function(){
       sinon.spy(badDancer, "step");
@@ -30,6 +24,20 @@ describe("badDancer", function() {
 
       clock.tick(timeBetweenSteps);
       expect(badDancer.step.callCount).to.be.equal(2);
+    });
+    it("should dance to a different location", function(){
+      sinon.spy(badDancer, "step");
+      expect(badDancer.step.callCount).to.be.equal(0);
+      var originalLeft = badDancer.left;
+      var originalTop = badDancer.top;
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(badDancer.step.callCount).to.be.equal(1);
+      var newLeft = badDancer.left;
+      var newTop = badDancer.top;
+      expect(newLeft).to.not.equal(originalLeft);
+      expect(newTop).to.not.equal(originalTop);
     });
   });
 });
