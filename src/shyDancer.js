4f0d39b7924f10f362c2e649add9861889e46211
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-19 21:24:30 -0700

    Added touched property to ShyDancer

--- src/shyDancer.js
+++ src/shyDancer.js
@@ -4,14 +4,15 @@ var ShyDancer = function(top, left, timeBetweenSteps){
 
   this.$node.addClass('shy-dancer');
   this.$node.mouseover(this.run.bind(this));
+  this.touched = 0;
 };
 
 ShyDancer.prototype = Object.create(Dancer.prototype);
 ShyDancer.prototype.constructor = ShyDancer;
 
 ShyDancer.prototype.run = function () {
+  this.touched++;  
   this.top += Math.random() * 300 - 150;
   this.left += Math.random() * 300 - 150;
-
   this.$node.animate({top: this.top, left: this.left});
 };
