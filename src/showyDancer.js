2305ba997598167ed7fca341752ac6e64d490fc0
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-19 18:26:43 -0700

    Adjusted how close showyDancers have to be from other dancers to start showing off.

--- src/showyDancer.js
+++ src/showyDancer.js
@@ -10,11 +10,10 @@ ShowyDancer.prototype = Object.create(BadDancer.prototype);
 ShowyDancer.prototype.constructor = ShowyDancer;
 
 ShowyDancer.prototype.step = function () {
-  BadDancer.prototype.step.call(this);
   var showingOff = false;
   for (var i = 0; i < window.dancers.length; i++) {
     if (this.dancersIndex !== i) {
-      if (this.distance(window.dancers[i].top, window.dancers[i].left) < 100) {
+      if (this.distance(window.dancers[i].top, window.dancers[i].left) < 125) {
         showingOff = true;
         break;
       }
@@ -25,6 +24,7 @@ ShowyDancer.prototype.step = function () {
   } else {
     this.$node.removeClass('show-off');
   }
+  BadDancer.prototype.step.call(this);
 };
 
 ShowyDancer.prototype.showOff = function () {
